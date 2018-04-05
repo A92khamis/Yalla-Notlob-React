02ec2374f32485dc78c2fb24c3fdeaf1530d2f75
AsSamhaa <assamhaa@gmail.com>
2018-04-05 08:56:25 +0200

    added login page

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import LogIn from './App';
+
+ReactDOM.render(
+  <LogIn />,
+  document.getElementById('root')
+);
